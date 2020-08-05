7041e93d08c130af4a0234f2715c4c303ef8cf95
Ariel Grosh <ariel241202@gmail.com>
2020-08-06 00:46:51 +0300

    Added diffrent error types

--- src/ErrorCode.h
+++ src/ErrorCode.h
@@ -4,11 +4,17 @@
 #include <stdint.h>
 
 typedef enum {
+<<<<<<< HEAD:src/ErrorCode.h
   // DON'T CHANGE THIS VALUE!
   ERROR_SUCCESS = 0,
   ERROR_FAILURE = 1,
   ERROR_FAILURE_CANT_ALLOCATE = 2, //allocation error
   ERROR_FAILURE_INPUT_ERROR = 3 //input error, the parameters are wrong
+=======
+    // DON'T CHANGE THIS VALUE!
+    ERROR_SUCCESS = 0,ERROR_FAILURE = 1, ERROR_FAILURE_CANT_ALLOCATE = 2,
+    ERROR_FAILURE_INPUT_ERROR = 3, ERROR_FAILURE_LOGIC = 4
+>>>>>>> Added diffrent error types:ErrorCode.h
 } ErrorCode;
 
 /**
